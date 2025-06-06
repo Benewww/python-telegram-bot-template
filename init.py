f3cbe95c5d35963b0788fdb3f1ec6a5d192d88ef
Benewww <167518911+Benewww@users.noreply.github.com>
2025-06-06 16:05:01 +0200

    Update init.py

--- init.py
+++ init.py
@@ -11,7 +11,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Lorem ipsum dolor sit amet. Et numquam rerum et consequatur tempore rem molestiae quas et magni mollitia et galisum omnis sed dolorem culpa. Est eius perferendis ut laudantium dolores qui tempore rerum nam rerum repellendus. Id quod rerum ab quis earum id neque maiores At praesentium modi nam veniam animi. Et debitis rerum non possimus ipsa sed officiis esse ut reprehenderit quos nam aliquid obcaecati et maxime beatae sed autem amet.")
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token("7647849717:AAE9ZvS3m7ToFZM-UGL71WnscKwZGTzWOVo").build()
+    app = ApplicationBuilder().token("YOUR_TOKEN_HERE").build()
 
     app.add_handler(CommandHandler("start", start))
 
